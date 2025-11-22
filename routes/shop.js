eb47c926b62aa1671fd7d59163f3ad86398a13cf
Rikuto <mikadorikuto@gmail.com>
2025-11-22 20:11:43 +0900

    update the code

--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+// Import the admin module which exports an object containing a 'products' array.
+// This allows the shop route to access products that were added via the admin route.
+// The admin module shares its products data by exporting { products } instead of just the router.
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
